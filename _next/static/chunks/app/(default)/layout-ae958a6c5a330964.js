8053ea9561104d048e8e48f62d1c1ccf5cb21449
Robert Schmid <r.schmid@outlook.com>
2024-01-31 00:21:13 +0100

    Update

---
+++ _next/static/chunks/app/(default)/layout-ae958a6c5a330964.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[534],{7853:function(n,e,u){Promise.resolve().then(u.bind(u,6965))}},function(n){n.O(0,[521,965,667,488,744],function(){return n(n.s=7853)}),_N_E=n.O()}]);
